dc94e0a27e4e3df0b3125cd2294c802b3010e9e5
Maxim <2048terrabit@gmail.com>
2019-01-21 12:36:19 +0500

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Log',
-    version='0.1',
+    version='0.2',
     packages=['log',],
     license='MIT',
     long_description=open('README.md').read(),
